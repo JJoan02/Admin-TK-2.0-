a3bc16a23ef986347f964cfa9b0b2097b0df3f98
Joan TK <149820143+JJoan02@users.noreply.github.com>
2024-12-04 01:47:50 -0500

    Add files via upload

---
+++ plugins/group-totag.js
@@ -0,0 +1,18 @@
+let handler = async (m, { conn, participants }) => {
+    
+    let users = participants.map(u => u.id).filter(v => v !== conn.user.jid)
+    
+    if (!m.quoted) return m.reply(`🚩 Responde a un mensaje.`)
+ let invisibleChar = '\u200B'
+    let message = invisibleChar + m.quoted.text
+    conn.sendMessage(m.chat, { text: message, mentions: users })
+}
+
+handler.help = ['tag']
+handler.tags = ['group']
+handler.command = /^(totag|tag)$/i
+
+handler.admin = true
+handler.group = true
+
+export default handler
